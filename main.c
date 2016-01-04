1dc67aa5e658ea904ec152b973eeb12701f1c61f
Muhammadreza Haghiri <haghiri75@gmail.com>
2016-01-04 14:17:50 +0330

    Main source

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include "kbd.h"
 #include "kheap.h"
 
-int main(struct multiboot *multiboot_ptr){
+int main(int argc, char** argv){
   char c = 'n';
   terminal_init(1, 15);
   terminal_write_on_line("NanOS version 0.0.2", 0, 12, 15);
